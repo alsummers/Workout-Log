702fbeb04d9e90608ac41fe0fd1d4d9755f40dde
Allison Summers <allisonsummers44@gmail.com>
2018-01-29 11:23:33 -0500

    validation_fixed env

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -1,21 +1,24 @@
+require('dotenv').config();
 
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var sequelize = require('./db');
-var User = sequelize.import(__dirname + '\\models\\user');
+
+var User = sequelize.import('./models/user');
 //Create table
 User.sync(); // sync( {force: true}), to drop then create each time the app starts!
-
-
-//sequelize.sync(); //To reset the table sync({ force: true })
 
 app.use(bodyParser.json());
 
 app.use(require('./middleware/headers'));
+app.use(require('./middleware/validate-session'));
 
-app.use('/api/login', require('./routes/session'));
+
 app.use('/api/user', require('./routes/user'));
+//login route
+app.use('/api/login', require('./routes/session'));
+
 
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
@@ -24,3 +27,4 @@ app.use('/api/test', function(req, res){
 app.listen(3000, function(){
 	console.log('App is listening on 3000.')
 });
+
