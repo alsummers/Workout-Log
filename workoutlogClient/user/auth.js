20fde452684a37ac33e91cbe095babddf77e15c7
Allison Summers <allisonsummers44@gmail.com>
2018-01-29 13:38:35 -0500

    Refactoring client

--- workoutlogClient/user/auth.js
+++ workoutlogClient/user/auth.js
@@ -1,40 +1,72 @@
-$(function(){
-    $.extend(WorkoutLog, {
-        //signup method
-        signup: function(){
-            //username password variables
-            var username = $('#su_username').val();
-            var password = $('#su_password').val();
-            //user object
-            var user = {
-                user: {
-                    username: username,
-                    password: password
-                }
-        };
-            //sign up post
-            var signup = $.ajax({
-                type: "POST",
-                url: WorkoutLog.API_BASE + "user",
-                data: JSON.stringify( user ),
+
+$(function() {
+    $.extend( WorkoutLog, {
+       signup: function() {
+             var username = $("#su_username").val();
+             var password = $("#su_password").val();
+             var user = {user:  {username: username, password: password }};
+             var signup = $.ajax({
+                type: "POST", 
+                url: WorkoutLog.API_BASE + "user", 
+                data: JSON.stringify(user), 
                 contentType: "application/json"
-            });
-            //sign up fail/done
-            signup.done(function(data){
-                if(data.sessionToken){
-                    WorkoutLog.setAuthHeader(data.sessionToken);
+             });
+             signup.done(function(data) {
+                if (data.sessionToken) {
+                   WorkoutLog.setAuthHeader(data.sessionToken);
+                   
                 }
                 $("#signup-modal").modal("hide");
-                $("#.disabled").removeClass("disabled");
+                $(".disabled").removeClass("disabled");
                 $("#loginout").text("Logout");
-            }).fail(function(){
-                $("su_error").text("There was an issue with sign up").show();
+                // go to define tab
+                $('.nav-tabs a[href="#define"]').tab('show');
+             })
+             .fail(function() {
+                $("#su_error").text("There was an issue with your username").show();
+             });
+       },
+ 
+       login: function() {
+            var username = $("#li_username").val();
+            var password = $("#li_password").val();
+            var user = {user:  {username: username, password: password }};
+            var login = $.ajax({
+                type: "POST", 
+                url: WorkoutLog.API_BASE + "login", 
+                data: JSON.stringify(user), 
+                contentType: "application/json"
             });
-        }
-
-        //login method
-
-        //loginoutmethod
+            login.done(function(data) {
+                if (data.sessionToken) {
+                WorkoutLog.setAuthHeader(data.sessionToken);
+                
+                }
+                // TODO: add logic to set user and auth token	
+                $("#login-modal").modal("hide");
+                $(".disabled").removeClass("disabled");
+                $("#loginout").text("Logout");
+            })
+            .fail(function() {
+                $("#li_error").text("There was an issue with your username or password").show();
+               });
+       },
+ 
+       loginout: function() {
+          if (window.localStorage.getItem("sessionToken")) {
+             window.localStorage.removeItem("sessionToken");
+             $("#loginout").text("Login");
+          }
+       }
     });
+ 
+    // bind events
+    $("#login").on("click", WorkoutLog.login);
     $("#signup").on("click", WorkoutLog.signup);
-});
+    $("#loginout").on("click", WorkoutLog.loginout);
+ 
+    if (window.localStorage.getItem("sessionToken")) {
+       $("#loginout").text("Logout");
+    }
+ 
+ });
