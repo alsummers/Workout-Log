23f17fbeb3db781df3cbc500070cb3a0f6090855
Allison Summers <allisonsummers44@gmail.com>
2018-01-29 09:10:13 -0500

    Bcrypt added

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -1,15 +1,22 @@
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var sequelize = require('./db');
-
 var User = sequelize.import(__dirname + '\\models\\user');
 //Create table
 User.sync(); // sync( {force: true}), to drop then create each time the app starts!
 
+
+//sequelize.sync(); //To reset the table sync({ force: true })
+
 app.use(bodyParser.json());
+
 app.use(require('./middleware/headers'));
+
+app.use('/api/login', require('./routes/session'));
 app.use('/api/user', require('./routes/user'));
+
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
 });
