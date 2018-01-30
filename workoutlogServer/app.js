074745e3f3f00cd152895bd6a028f1e00f6f5ac8
Allison Summers <allisonsummers44@gmail.com>
2018-01-29 22:02:54 -0500

    Finalized logs

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -5,30 +5,23 @@ var app = express();
 var bodyParser = require('body-parser');
 var sequelize = require('./db');
 
-var User = sequelize.import('./models/user');
-//Create table
-// User.sync(); 
-// User.sync( {force: true}); //, to drop then create each time the app starts!
-sequelize.sync();
+sequelize.sync(); // tip: {force: true} for resetting tables
 
 app.use(bodyParser.json());
 
 app.use(require('./middleware/headers'));
 app.use(require('./middleware/validate-session'));
 
-
-app.use('/api/user', require('./routes/user'));
-//login route
-app.use('/api/login', require('./routes/session'));
-app.use('/api/definition', require('./routes/definition'));
-app.use('/api/log', require('./routes/log'));
-
-
-app.use('/api/test', function(req, res){
-	res.send("Hello World");
-});
-
 app.listen(3000, function(){
 	console.log('App is listening on 3000.')
+});
+
+
+app.use('/api/user', require('./routes/user'));
+app.use('/api/login', require('./routes/session'));
+app.use('/api/log', require('./routes/log'));
+app.use('/api/definition', require('./routes/definition'));
+app.use('/api/test', function(req, res){
+	res.send("Hello World");
 });
 
