293060719157bcfad024be9c4f122a2c5e10236a
Allison Summers <allisonsummers44@gmail.com>
2018-02-05 11:09:27 -0500

    fixed login font color, fluid input type

--- workoutlogClient/workouts/water.js
+++ workoutlogClient/workouts/water.js
@@ -3,72 +3,72 @@ $(function() {
 		water: {
 			userIntake: [],
 
-			create: function() {
+				create: function() {
 
-				var fluid = { 
-		         		amount: $("#fluid-intake").val(),
-				};
-				var postData = { water: fluid };
-		      	var waterIntake = $.ajax({
+					var fluid = { 
+							amount: $("#fluid-intake").val(),
+					};
+					var postData = { water: fluid };
+					var waterIntake = $.ajax({
 
-         	type: "POST",
-	         	url: WorkoutLog.API_BASE + "water",
-	         	data: JSON.stringify(postData),
-	         	contentType: "application/json"
-		      	});
+				type: "POST",
+					url: WorkoutLog.API_BASE + "water",
+					data: JSON.stringify(postData),
+					contentType: "application/json"
+					});
 
-		      	waterIntake.done(function(data) {
-	      			WorkoutLog.water.userIntake.push(data.water);
-					$("#fluid-intake").val("");
-					$('a[href="#history"]').tab("show");
+					waterIntake.done(function(data) {
+						WorkoutLog.water.userIntake.push(data.water);
+						$("#fluid-intake").val("");
+						$('a[href="#history"]').tab("show");
 
-		      	});
-		  },
+					});
+			},
 
-		  fetchAll: function() {
-			 var fetchDefs = $.ajax({
-		         type: "GET",
-		         url: WorkoutLog.API_BASE + "water",
-		         headers: {
-		         	"authorization": window.localStorage.getItem("sessionToken")
-		         }
-		      })
-		      .done(function(data) {
-		         WorkoutLog.water.userIntake = data;
-		      })
-		      .fail(function(err) {
-		         console.log(err);
-		      });
-		  },
+			fetchAll: function() {
+				var fetchDefs = $.ajax({
+					type: "GET",
+					url: WorkoutLog.API_BASE + "water",
+					headers: {
+						"authorization": window.localStorage.getItem("sessionToken")
+					}
+				})
+				.done(function(data) {
+					WorkoutLog.water.userIntake = data;
+				})
+				.fail(function(err) {
+					console.log(err);
+				});
+			},
 
-		  getChart: function() {
-			  var ctx = $("#progressChart");
-			  var newChart = new Chart(ctx, {
-				  type: 'bar',
-				  data: {
-					  labels: ["oz."],
-					  datasets: [{
-						  label: "Fluid Intake",
-						  data: [WorkoutLog.water.userIntake],
-						  backgroundColor: [
-							  'rgba(0, 191, 255, 0.2'
-						  ],
-						  borderColor: [
-							  'rgba(0, 191, 255, 1)'
-						  ],
-						  borderWidth: 2
-					  }]
-				  },
-				  options: {
-					  scales: {
-						  yAxes: [{
-							  ticks: {
-								beginAtZero: true
-							  }
-						  }]
-					  }
-				  }
-			  })
+			getChart: function() {
+				var ctx = $("#progressChart");
+				var newChart = new Chart(ctx, {
+					type: 'bar',
+					data: {
+						labels: ["oz."],
+						datasets: [{
+							label: "Fluid Intake",
+							data: [WorkoutLog.water.userIntake],
+							backgroundColor: [
+								'rgba(0, 191, 255, 0.2'
+							],
+							borderColor: [
+								'rgba(0, 191, 255, 1)'
+							],
+							borderWidth: 2
+						}]
+					},
+					options: {
+						scales: {
+							yAxes: [{
+								ticks: {
+									beginAtZero: true
+								}
+							}]
+						}
+					}
+				})
 		  }
 		}
 		
